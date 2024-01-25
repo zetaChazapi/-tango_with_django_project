839f8e9bccb1953902848f4bea9ee9dda8fe6177
“Zeta <zetachazapi@pc-4-100.customer.ask4.lan>
2024-01-25 21:04:45 +0000

    up to chapter 5

--- rango/views.py
+++ rango/views.py
@@ -5,13 +5,16 @@ from django.shortcuts import render
 # the page we wish to send to the client requesting the view
 
 
-def index(request):
-    html_content = (
-        '<a href="/rango/about/">About</a><br>Rango says Hey welcome to the Homepage!'
-    )
-    return HttpResponse(html_content)
-
-
 def about(request):
-    html_content = '<a href= "/rango/">Index</a><br>This is the About Page!'
-    return HttpResponse(html_content)
+    about_context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, "rango/about.html", context=about_context_dict)
+
+
+def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    index_context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, "rango/index.html", context=index_context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("rango/", include("rango.urls")),
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
