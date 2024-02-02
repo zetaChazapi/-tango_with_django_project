e6eb34cfc616426b96e4da41f498f19042dedecf
“Zeta <zetachazapi@glaroam2-145-82.wireless.gla.ac.uk>
2024-02-02 18:00:13 +0000

    Chapter 10 complete - passed

--- rango/views.py
+++ rango/views.py
@@ -9,22 +9,58 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, "visits", "1"))
+    last_visit_cookie = get_server_side_cookie(
+        request, "last_visit", str(datetime.now())
+    )
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session["last_visit"] = str(datetime.now())
+
+    else:
+        request.session["last_visit"] = last_visit_cookie
+
+    request.session["visits"] = visits
 
 
 def about(request):
     about_context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+    visitor_cookie_handler(request)
+    about_context_dict["visits"] = request.session["visits"]
+
     return render(request, "rango/about.html", context=about_context_dict)
 
 
 def index(request):
     category_list = Category.objects.order_by("-likes")[:5]
     page_list = Page.objects.order_by("-views")[:5]
-    context_dict = {
-        "boldmessage": "Crunchy, creamy, cookie, candy, cupcake!",
-        "categories": category_list,
-        "pages": page_list,
-    }
-    return render(request, "rango/index.html", context=context_dict)
+
+    context_dict = {}
+    context_dict["boldmessage"] = "Crunchy, creamy, cookie, candy, cupcake!"
+    context_dict["categories"] = category_list
+    context_dict["pages"] = page_list
+
+    visitor_cookie_handler(request)
+
+    response = render(request, "rango/index.html", context=context_dict)
+    return response
 
 
 def show_category(request, category_name_slug):
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 LOGIN_URL = "rango:login"
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Application definition
 
